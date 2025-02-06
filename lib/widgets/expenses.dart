83b140ea64c3c6ffcf6f14be9393126a6ee194a3
uche-hub <ucj.theuche@gmail.com>
2025-02-06 01:02:01 +0100

    Working on Model bottom Sheet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -28,9 +29,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) {
-        return Text("Model Bottom Sheet");
-      },
+      builder: (ctx) => NewExpense(),
     );
   }
 
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,8 +8,38 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = ' ';
+
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            onChanged: _saveTitleInput,
+            maxLength: 50,
+            decoration: InputDecoration(
+                label: Text(
+              'Title',
+            )),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_enteredTitle);
+                },
+                child: Text("Save Expense"),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
